04f04a5e066af903b700b313e593837863bd55e4
e-bondarev <82723220+e-bondarev@users.noreply.github.com>
2021-08-10 12:42:45 +0200

    glfw and glew added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+
 int main()
 {
-	std::cout << "Hello, world!\n";
+	glfwInit();
+	glfwDefaultWindowHints();
+
+	window = glfwCreateWindow(800, 600, "ide", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
